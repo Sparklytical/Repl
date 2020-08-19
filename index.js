0b06426e3708cc0b2365faccf65202db18d5b7fd
Kamran Tahir <kamrantahir25@gmail.com>
2020-08-19 05:07:59 +0000

    anagram

---
+++ index.js
@@ -0,0 +1,24 @@
+function anagram(str1,str2) {
+  if (str1.length!== str2.length) {
+    return false;
+  }
+
+  const lookup={}
+
+  for(let i=0;i<str1.length;i++) {
+    let letter=str1[i];
+    lookup[letter]?lookup[letter]+=1: lookup[letter]=1;
+  }
+
+  for(let i=0;i<str2.length;i++) {
+    let letter=str2[i];
+    if(!lookup[letter]) {
+      return false
+    } else {
+      lookup[letter]=-1
+    }
+  }
+  return true
+}
+
+anagram('kamran','narmak')
